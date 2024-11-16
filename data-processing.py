4dc6a18176703e6af689c9019432706260c319d2
Andrew Wang <andrew_wang2002@hotmail.com>
2024-11-16 18:02:05 -0500

    processing title

--- data-processing.py
+++ data-processing.py
@@ -1,5 +1,7 @@
+import re
 import pandas as pd
 from PIL import Image
+
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize.treebank import TreebankWordDetokenizer, TreebankWordTokenizer
@@ -12,19 +14,25 @@ file_processed = "data-processed.csv"
 df = pd.read_csv(filepath, index_col="yt-id")
 
 # Create new dataframe for processed data
-processed_df = df[["title"]]
+processed_df = df[["title", "view-count"]]
 
 # Remove meaningless words and emoji from title
 token = TreebankWordTokenizer()
 detoken = TreebankWordDetokenizer()
 nltk.download('stopwords')
 stopwords_list = stopwords.words("english")
+
+emoji_re = re.compile(pattern="[\U000000A9-\U0010ffff]", flags = re.UNICODE)
 def filter_title(x):
-	words = token.tokenize(x["title"].lower())
-	print(x["title"])
+	# Remove Emojis
+	raw_title = emoji_re.sub(r'', x["title"])
+
+	# Remove insignificant words
+	words = token.tokenize(raw_title.lower())
 	filter_words = [word for word in words if word not in stopwords_list]
 	x["title"] = detoken.detokenize(filter_words)
-	print(x["title"])
 	return x
 
-processed_df.apply(filter_title, axis=1)
+processed_df = processed_df.apply(filter_title, axis=1, result_type='broadcast')
+
+processed_df.to_csv(file_processed)
