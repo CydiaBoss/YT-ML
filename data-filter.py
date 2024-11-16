642ce655b5672d766e92f962c7bab7ba1b780c4f
Andrew Wang <andrew_wang2002@hotmail.com>
2024-11-15 22:09:35 -0500

    add translation filter

--- data-filter.py
+++ data-filter.py
@@ -1,10 +1,12 @@
 import pandas as pd
-from googletrans import Translator
+from langdetect import detect
+from langdetect.lang_detect_exception import LangDetectException
 
 # Constants
 MULT_CSV = False
 filepath = "data.csv"
 filepath_2 = "data_2.csv"
+filepath_final = "data-filtered.csv"
 lang = "en"
 
 # Read Data
@@ -15,5 +17,22 @@ if MULT_CSV:
 	df_2 = pd.read_csv(filepath_2, index_col="yt-id")
 	df = pd.concat([df, df_2])
 
-# Filtering data
-df = df.drop_duplicates("yt-id")
+print(f"{df.size} rows in data file")
+
+# Remove duplicates
+df = df[~df.index.duplicated(keep='first')]
+print(f"{df.size} rows remaining after duplication filter")
+
+# Remove non language
+def lang_filter(row) -> bool:
+	try:
+		print(row["title"])
+		return detect(row["title"]) == lang
+	except LangDetectException:
+		return False
+	
+df = df[df.apply(lang_filter, axis=1)]
+print(f"{df.size} rows remaining after translation filter")
+
+# Save Filtered Data
+df.to_csv(filepath_final)
